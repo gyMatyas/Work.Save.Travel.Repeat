038aa6f15797ee05c198924652625230603326e5
György Mátyás <gy.matyas97@gmail.com>
2018-05-08 14:40:24 +0200

    crossorigin

--- src/main/java/com/gymatyas/wstr/Models/Article.java
+++ src/main/java/com/gymatyas/wstr/Models/Article.java
@@ -1,6 +1,5 @@
 package com.gymatyas.wstr.Models;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.Column;
